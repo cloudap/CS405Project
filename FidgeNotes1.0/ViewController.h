17eb0ec9d6249810a6d65558e188d0c7272ac5c0
Alicia Pixton <cookiemonster.four@gmail.com>
2014-10-10 10:42:00 -0600

    Initial Commit

---
+++ FidgeNotes1.0/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FidgeNotes1.0
+//
+//  Created by Alicia Pixton on 10/10/14.
+//  Copyright (c) 2014 TheFridge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FidgeNotes1.0/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FidgeNotes1.0
+//
+//  Created by Alicia Pixton on 10/10/14.
+//  Copyright (c) 2014 TheFridge. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
